35b8fd0d70ad52c1499e281073d2a653b5f4a87c
Tigran Harutyunyan <harutyunyan.tigran.v@gmail.com>
2023-07-05 10:35:32 +0000

    update v2

--- app.py
+++ app.py
@@ -293,6 +293,6 @@ def my_job_list():
 #     return jsonify(success=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    # port = int(os.environ.get('PORT', 5000))
-    # app.run(host='0.0.0.0', port=port)
+    # app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
